ac7b280280621c0d31171c1d58ecc5c33f3a0475
Nostro37 <nostro37@gmail.com>
2022-03-14 16:39:10 -0400

    3:32:19

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
-const { readFile, writeFile } = require('fs').promises;
-// const util = require('util');
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
+const { createReadStream } = require('fs');
 
+const stream = createReadStream('./content/big.txt');
 
-const start = async() => {
-    try {
-        const first = await readFile('./content/first.txt', 'utf8')
-        const second = await readFile('./content/second.txt', 'utf8')
-        await writeFile('./content/result-mind-grenade.txt', `This is awesome : ${first} ${second}`, { flag: 'a' } )
-        console.log(first, second)
-    } catch (error) {
-        console.log(error);
-    }
-    
-}
-
-start()
-
-
-
-
-// const getText = (path) => {
-//     return new Promise((resolve, reject) => {
-//         readFile(path, 'utf8', (err, data) => {
-//             if (err) {
-//                 reject(err)
-//             } else {
-//                 resolve(data)
-//             }
-//         })
-//     })
-// }
-
-
-// getText('./content/first.txt')
-// .then(result => console.log(result))
-// .catch((err) => console.log(err))
-
+stream.on('data', (result) => {
+    console.log(result)
+} )
